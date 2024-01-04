cca409d74ca9ae2d1586d8b2e16b7510f8bddf4f
NukeThief <81801547+NukeThief@users.noreply.github.com>
2024-01-04 20:57:32 +0100

    Update keep_alive.py

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -4,8 +4,3 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "I am alive and well :)"
-def run():
-  app.run(host='0.0.0.0',port=8080)
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
